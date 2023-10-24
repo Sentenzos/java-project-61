1f3d9cb51601092990acb8245db51b49778f0526
sentenzos <sentenzoss@gmail.com>
2023-10-24 16:26:22 +0300

    build.gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -16,4 +17,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+compileJava {
+    options.release = 21
 }
