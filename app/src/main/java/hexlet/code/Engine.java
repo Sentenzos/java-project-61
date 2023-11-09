00425b9a4895f7422e6f4c869003cf01be9a9ed7
sentenzos <sentenzoss@gmail.com>
2023-11-09 12:41:36 +0300

    fix

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -41,7 +41,7 @@ public class Engine {
             System.out.println(
                     "'" + answer + "'" + " is wrong answer ;(. Correct answer was " + correctAnswer
             );
-            System.out.println("Let's try again, " + username);
+            System.out.println("Let's try again, " + username + "!");
 
             return false;
         }
