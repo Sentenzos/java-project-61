fb49b7ee11a6617f2a60421660e816a6723ddc9b
sentenzos <sentenzoss@gmail.com>
2023-11-09 11:37:59 +0300

    Refactoring

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,47 +4,52 @@ import hexlet.code.Engine;
 
 public class Progression {
     public static void run() {
-        var username = Engine.greet();
         Engine.printRules("What number is missing in the progression?");
 
         for (var i = 0; i < 3; i++) {
-            var maxStartNumber = 25;
-            int maxStep = 10;
-            int progressionLength = 10;
-            int startNumber = (int) (Math.random() * maxStartNumber + 1);
-            int step = Engine.getRandomInt(1, maxStep);
-            int skippedStep = Engine.getRandomInt(1, maxStep);
-
-            String progressionNumbersString = "";
-            var resultNumber = 0;
-            var correctAnswer = 0;
-
-            for (var j = 0; j < progressionLength; j++) {
-                if (j == 0) {
-                    resultNumber += startNumber;
-                    if (skippedStep == 0) {
-                        progressionNumbersString = "..";
-                        correctAnswer = resultNumber;
-                    } else {
-                        progressionNumbersString += startNumber;
-                    }
-                } else if (j == skippedStep - 1) {
-                    progressionNumbersString += " ..";
-                    correctAnswer = resultNumber + step;
-                    resultNumber += step;
-                } else {
-                    progressionNumbersString += " " + (resultNumber + step);
-                    resultNumber += step;
-                }
-            }
-
-            var roundResult = Engine.handleRound(progressionNumbersString,
-                    Integer.toString(correctAnswer), i, username);
+            var roundResult = Progression.process(i);
 
             if (!roundResult) {
                 break;
             }
         }
+    }
+
+    public static boolean process(int roundNumber) {
+        var username = Engine.greet();
+
+        var maxStartNumber = 25;
+        int maxStep = 10;
+        int progressionLength = 10;
+        int startNumber = (int) (Math.random() * maxStartNumber + 1);
+        int step = Engine.getRandomInt(1, maxStep);
+        int skippedStep = Engine.getRandomInt(1, maxStep);
+
+        String progressionNumbersString = "";
+        var resultNumber = 0;
+        var correctAnswer = 0;
+
+        for (var j = 0; j < progressionLength; j++) {
+            if (j == 0) {
+                resultNumber += startNumber;
+                if (skippedStep == 0) {
+                    progressionNumbersString = "..";
+                    correctAnswer = resultNumber;
+                } else {
+                    progressionNumbersString += startNumber;
+                }
+            } else if (j == skippedStep - 1) {
+                progressionNumbersString += " ..";
+                correctAnswer = resultNumber + step;
+                resultNumber += step;
+            } else {
+                progressionNumbersString += " " + (resultNumber + step);
+                resultNumber += step;
+            }
+        }
+
+        return Engine.handleRound(progressionNumbersString,
+                Integer.toString(correctAnswer), roundNumber, username);
     }
 }
 
